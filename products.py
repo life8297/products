246510f71130fa68445b4acac1af1f232deb574c
JR <allen8297@gmail.com>
2020-07-12 11:38:19 +0800

    add price and 2-dimensional list

--- products.py
+++ products.py
@@ -19,5 +19,5 @@ while True: # 未知範圍常用while True; 已知範圍用for搭配range
 print(products)
 
 # 存取二維清單
-products[0][0] # 大清單中的第0資料, 中的小清單第0個資料
-products[1][1] # 大清單中的第1資料, 中的小清單第1個資料
+# products[0][0] # 大清單中的第0資料, 中的小清單第0個資料
+# products[1][1] # 大清單中的第1資料, 中的小清單第1個資料
