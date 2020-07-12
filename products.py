74044e8a230f04f876b7721ab8e83ae544e34d93
JR <allen8297@gmail.com>
2020-07-12 15:30:33 +0800

    add title and use utf-8

--- products.py
+++ products.py
@@ -26,6 +26,7 @@ print(products)
 for p in products: # 利用for拆解第一層清單
 	print(p[0], '的價格為:', p[1], '元')
 
-with open('products.csv', 'w') as f: # 打開檔案
+with open('products.csv', 'w', encoding = 'utf-8') as f: # 打開檔案 # 限定編碼模式
+	f.write('商品, 價格\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '元' + '\n') # 寫入檔案 # 字串的合併
